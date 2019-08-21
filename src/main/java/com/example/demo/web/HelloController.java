f75cc7cbf0e917a00f5eb194c3799a60774306e6
liyonghui <liyonghui@cdvcloud.com>
2019-08-21 21:17:24 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo.web;
+
+import com.example.demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:lyh
+ * @Description:
+ * @Date:Created in 2019/8/21 21:04
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/sayhi")
+    public String sayhi(@RequestParam String name) {
+        return helloService.hiService(name);
+    }
+}
